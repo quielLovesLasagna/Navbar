1c613acd0b774df53b3147c644573ec89a26982e
Exequiel Arco <137391972+quielloveslasana@users.noreply.github.com>
2023-10-10 08:55:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", () => links.classList.toggle("show-links"));
