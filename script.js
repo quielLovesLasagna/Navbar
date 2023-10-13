5f9c45c324bac27434c67f1750e93deaec1be9aa
Exequiel Arco <exequielarco23@gmail.com>
2023-10-13 21:28:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 "use strict";
 
+// ---- ELEMENTS ---- //
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
+// ---- END OF ELEMENTS ---- //
 
+// ---- EVENT HANDLER ---- //
 navToggle.addEventListener("click", () => links.classList.toggle("show-links"));
+// ---- END OF EVENT HANDLER ---- //
